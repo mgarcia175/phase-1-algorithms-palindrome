adb17f64958fe233c9cd1cc7d15a52f9e37bbaa7
Martin <martingarcia804@yahoo.com>
2023-03-10 15:45:06 -0600

    Completed Lab

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-function isPalindrome(word) {
+function isPalindrome(string) {
   // Write your algorithm here
+  const len = string.length
+
+  for(let i = 0; i < len / 2; i++) {
+    if (string[i] !== string[len - 1 - i]) {
+      return false
+    }
+  }
+  return true
 }
 
 /* 
   Add your pseudocode here
+  if (the string is a palindrome, meaning the opposite syntax of a letter in a string, is the same as it's counterpart)
+  {return true} else {false}
 */
 
 /*
   Add written explanation of your solution here
+  String should be converted to lowercase in order to account for any case sensitive differences
 */
 
 // You can run `node index.js` to view these console logs
